e4629640ccc553b6da0c2cfb88fb2ed8283643c6
Pietro Visconti <visconti373@gmail.com>
2024-11-28 10:31:00 +0100

    Remove stuff

--- celestia-node-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-NETWORK=celestia
-NODE_TYPE=light
-RPC_URL=rpc.celestia.pops.one
-docker run -e NODE_TYPE=$NODE_TYPE -e P2P_NETWORK=$NETWORK \
-    -v ./node-data:/home/celestia \
-    ghcr.io/celestiaorg/celestia-node:v0.20.2 \
-    celestia $NODE_TYPE start --core.ip $RPC_URL --p2p.network $NETWORK
