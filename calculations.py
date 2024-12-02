11348e483349a1f85599819893d6a34221242cf7
Pietro Visconti <visconti373@gmail.com>
2024-12-02 16:56:09 +0100

    Fix values

--- calculations.py
+++ calculations.py
@@ -5,28 +5,45 @@ t1F = 3.0
 t1S = 25.73
 
 T = 3.0
-
-w1FF = round(1 - math.exp(-T / t1FF), 4)
-w1F = round(1 - math.exp(-T / t1F), 4)
-w1S = round(1 - math.exp(-T / t1S), 4)
-
-print("w1FF = ", w1FF)
-print("w1F = ", w1F)
-print("w1S = ", w1S)
-
 T2 = 1
-t2 = 1
-w2 = round(1 - math.exp(-T2 / t2), 4)
-
-print("w2 = ", w2)
-
-beta = 1 / T2
-print("beta = ", beta)
-
 T3 = 1
-delta = 1 / T3
-print("delta = ", delta)
-
 T4 = 1
-eta = 1 / T4
-print("eta = ", eta)
+g = 0.5
+
+for i in range(1, 7):
+    print('\n')
+    print("ROUND ", i)
+
+
+
+    w1FF = round(1 - math.exp(-T / t1FF), 4)
+    w1F = round(1 - math.exp(-T / t1F), 4)
+    w1S = round(1 - math.exp(-T / t1S), 4)
+
+    gammaFF = round(max(1/t1FF, 1/(T + (i-1)*g)), 4)
+    gammaF = round(max(1/t1F, 1/(T + (i-1)*g)), 4)
+    gammaS = round(max(1/t1S, 1/(T + (i-1)*g)), 4)
+
+    print("w1FF =", w1FF)
+    print("gammaFF =", gammaFF)
+    print("w1F =", w1F)
+    print("gammaF =", gammaF)
+    print("w1S =", w1S)
+    print("gammaS =", gammaS)
+
+    t2 = 1
+    w2 = round(1 - math.exp(-T2 / t2), 4)
+
+    print("w2 =", w2)
+
+    beta = round(max(1/t2, 1/(T2 + (i-1)*g)), 4)
+    print("beta =", beta)
+
+    delta = round(1/(T3 + (i-1)*g), 4)
+    print("delta =", delta)
+
+    w3 = 0.999
+    print("w3 =", w3)
+
+    eta = 1 / T4
+    print("eta =", eta)
