421b4e1ceb03c304a9c09a22655c047ace453d23
Pietro Visconti <visconti373@gmail.com>
2024-12-05 14:05:48 +0100

    Fix models

--- calculations-celestia.py
+++ calculations-celestia.py
@@ -4,9 +4,15 @@ t1FF = 1.14
 t1F = 3.0
 t1S = 25.73
 
+t2 = 2
+
 t1FF *= 2
 t1F *= 2
 t1S *= 2
+
+print("t1FF =", t1FF)
+print("t1F =", t1F)
+print("t1S =", t1S)
 
 T = 3.0
 T2 = 1
@@ -14,38 +20,26 @@ T3 = 1
 T4 = 1
 g = 0.5
 
-for i in range(1, 7):
-    print('\n')
-    print("ROUND ", i)
+T *= 2
+T2 *= 2
+T3 *= 2
+T4 *= 2
 
-    w1FF = round(1 - math.exp(-T / t1FF), 4)
-    w1F = round(1 - math.exp(-T / t1F), 4)
-    w1S = round(1 - math.exp(-T / t1S), 4)
+w1FF = round(1 - math.exp(-T / t1FF), 4)
+w1F = round(1 - math.exp(-T / t1F), 4)
+w1S = round(1 - math.exp(-T / t1S), 4)
 
-    gammaFF = round(max(1/t1FF, 1/(T + (i-1)*g)), 4)
-    gammaF = round(max(1/t1F, 1/(T + (i-1)*g)), 4)
-    gammaS = round(max(1/t1S, 1/(T + (i-1)*g)), 4)
+print("w1FF =", w1FF)
+print("w1F =", w1F)
+print("w1S =", w1S)
 
-    print("w1FF =", w1FF)
-    print("gammaFF =", gammaFF)
-    print("w1F =", w1F)
-    print("gammaF =", gammaF)
-    print("w1S =", w1S)
-    print("gammaS =", gammaS)
+w2 = round(1 - math.exp(-T2 / t2), 4)
+print("w2 =", w2)
 
-    t2 = 1
-    w2 = round(1 - math.exp(-T2 / t2), 4)
+w3 = 0.999
+print("w3 =", w3)
 
-    print("w2 =", w2)
+print("------------------------------------")
 
-    beta = round(max(1/t2, 1/(T2 + (i-1)*g)), 4)
-    print("beta =", beta)
-
-    delta = round(1/(T3 + (i-1)*g), 4)
-    print("delta =", delta)
-
-    w3 = 0.999
-    print("w3 =", w3)
-
-    eta = 1 / T4
-    print("eta =", eta)
+eta = 1 / T4
+print("eta =", eta)
